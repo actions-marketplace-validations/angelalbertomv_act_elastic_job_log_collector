7eb00fddbc43c89bcd36a190910c872187f47f3e
angelalbertomv <angelalbertomv@hotmail.com>
2021-02-25 11:25:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,39 +6,38 @@ def main():
     
     my_input = os.environ["INPUT_MYINPUT"]
     
-    GITHUB_JOB = os.environ["GITHUB_JOB"]
-    
-    print(GITHUB_JOB)
-    
-    GITHUB_REF = os.environ["GITHUB_REF"]
-    
-    print(GITHUB_REF)
-        
+    GITHUB_JOB = os.environ["GITHUB_JOB"]    
+    print(GITHUB_JOB)    
+    GITHUB_REF = os.environ["GITHUB_REF"]    
+    print(GITHUB_REF)        
     GITHUB_REPOSITORY = os.environ["GITHUB_REPOSITORY"]    
+    print(GITHUB_REPOSITORY)    
     GITHUB_REPOSITORY_OWNER = os.environ["GITHUB_REPOSITORY_OWNER"]
+    print(GITHUB_REPOSITORY_OWNER)    
     GITHUB_RUN_ID = os.environ["GITHUB_RUN_ID"]     
     print(GITHUB_RUN_ID)
     GITHUB_RUN_NUMBER = os.environ["GITHUB_RUN_NUMBER"]     
-
+    print(GITHUB_RUN_NUMBER)
     GITHUB_WORKFLOW = os.environ["GITHUB_WORKFLOW"]
-    
     print(GITHUB_WORKFLOW)
-
     GITHUB_SERVER_URL = os.environ["GITHUB_SERVER_URL"]
-    
     print(GITHUB_SERVER_URL)
-    
     GITHUB_API_URL = os.environ["GITHUB_API_URL"]
-    
     print(GITHUB_API_URL)
     GITHUB_ACTION = os.environ["GITHUB_ACTION"]
-
+    print(GITHUB_ACTION)
     ACTIONS_RUNTIME_URL = os.environ["ACTIONS_RUNTIME_URL"]
     print(ACTIONS_RUNTIME_URL)
     ACTIONS_RUNTIME_TOKEN = os.environ["ACTIONS_RUNTIME_TOKEN"]
     print(ACTIONS_RUNTIME_TOKEN)
     
     url = "{url}/repos/{owner}/{repo}/actions/runs/{run_id}".format(url=ACTIONS_RUNTIME_URL,owner=GITHUB_REPOSITORY_OWNER,repo=GITHUB_REPOSITORY,run_id=GITHUB_RUN_ID)
+    
+    print(url)
+    
+    r = requests.get(url)
+    
+    url = "{url}/repos/{owner}/{repo}/actions/runs/{run_id}".format(url=ACTIONS_RUNTIME_URL,owner=GITHUB_REPOSITORY_OWNER,repo='azure',run_id=GITHUB_RUN_ID)
     
     print(url)
     
@@ -54,6 +53,15 @@ def main():
     
     print(str(r))
 
+    url = "{url}/repos/{owner}/{repo}/actions/runs/{run_id}".format(url=GITHUB_SERVER_URL,owner=GITHUB_REPOSITORY_OWNER,repo='azure',run_id=GITHUB_RUN_ID)
+    
+    print(url)    
+    
+    r = requests.get(url)
+    
+    print(str(r))
+    
+    
     my_output = f"Hello {my_input}"       
 
     print(f"::set-output name=myOutput::{my_output}")
